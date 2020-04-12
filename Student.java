1f8732e1f1d8ec9fb84c0ed4281efec8047fb853
Angel <angel.kotevski@students.finki.ukim.mk>
2020-04-12 20:48:48 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -4,14 +4,47 @@ class Student {
 	String lastName;
 	int grades=[6,6,7,8,5,8,9,10,8,9,7,7]
 	//TODO constructor
-
+	 StudentString index, String firstName,String lastName,int grades)
+	{
+		this.index=index;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.grades=grades
+	}
 	//TODO seters & getters
+	void setthemAll(index,firstName,lastName,grades)
+	{
+		this.index=index;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.grades=grades
+	}
+	String vratiIndeks()
+	{return this.index;}
+	String vratiIme(){return this.firstName;}
+	String vratiPrz(){return this.lastName;}
+	int vratiNiza(){return this.grades;}
 
 	public double getAverage() {
 		//TODO
+	int sum=0
+	for(int i=0; i< len(this.grades);i++)
+	{
+		sum+=this.grades[i];
+	}
+	return (sum/len(this.grades)
 	}
 
 	public int ECTSCredits() {
 		//TODO
+	kolku=0;
+	for(int i=0;i<len(this.grades);i++)
+	{
+		if(this.grades[i]>5)
+		{
+			kolku+=6;
+		}
+	}
+	return kolku;
 	}
 }
